1dbe2f2de7fa485048e8e8c2b983815c343585c8
Annika Neumann <annika1983neumann@gmail.com>
2022-04-14 19:12:50 +0100

    Update title

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TitleBar = ({handleSelectChange, genres}) =>{
   return (
     <div>
-      <h1>Hit Parade</h1>
+      <h1>CodeClan Hit Parade</h1>
       <select onChange={handleSelectChange}>
         {genres.map(genre => {
           return <option key={genre.name} value={genre.url}>{genre.name}</option>
